e21ca57d2ca9d1f2e6451fdcf239f161d18bb5c7
OscarRamosRivas <oscar.ramos.rivas@gmail.com>
2018-12-02 11:32:20 +0100

    Variables en PHP

---
+++ Variables.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Aqui podemos hacer un comentario de varias lineas
+*/
+//Comentario una sola linea
+$uno = "Esto es una variable tipo cadena";
+
+//Variable tipo integer(numero entero)
+$dos = 100;
+
+//Variable tipo float(numero con decimales)
+$tres = 3.14;
+//Para mostrarlo en pantalla utilizamos echo
+echo $uno;
+//Pintamos una linea horizontal con hr
+echo "<hr>";
+echo $dos;
+//Creamos un salto de linea con br
+echo "<br>";
+echo $tres;
+
+?>
