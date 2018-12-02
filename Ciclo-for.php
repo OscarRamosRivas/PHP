d755e9dbd5daf988ac96cf8675820b1e6db1dbba
OscarRamosRivas <oscar.ramos.rivas@gmail.com>
2018-12-02 12:37:54 +0100

    Bucle for

---
+++ Ciclo-for.php
@@ -0,0 +1,24 @@
+<?php
+/*
+    El ciclo for lo utilizamos para hacer un número de repeticiones.
+    Para ello necesitamos el valor inicial($i), una condición(la que va 
+    a indicar al bucle que termine) y el valor de incremento o decremento.
+*/
+
+//Contar del 1 al 10
+for ($i=1; $i <= 10; $i++) { 
+    echo $i."<br>";
+}
+
+//Tambien podemos contar hacia atras
+for ($x=10; $x >=1 ; $x--) { 
+    echo $x."<br>";
+}
+
+/*Podemos indicar el salto de número
+Aqui nos contará de 3 en 3 hasta 30
+*/
+for ($z=0; $z <= 30; $z= $z + 3) { 
+    echo " - ".$z;
+}
+?>
--- Variables.php
+++ Variables.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Aqui podemos hacer un comentario de varias lineas
+Aquí podemos hacer un comentario de varias lineas
 */
 //Comentario una sola linea
 $uno = "Esto es una variable tipo cadena";
@@ -10,6 +10,11 @@ $dos = 100;
 
 //Variable tipo float(numero con decimales)
 $tres = 3.14;
+
+//Variable tipo booleano
+$cuatro = true;
+
+
 //Para mostrarlo en pantalla utilizamos echo
 echo $uno;
 //Pintamos una linea horizontal con hr
@@ -18,5 +23,8 @@ echo $dos;
 //Creamos un salto de linea con br
 echo "<br>";
 echo $tres;
+echo "<br>";
+echo "Verdadero: ".$cuatro; 
+
 
 ?>
